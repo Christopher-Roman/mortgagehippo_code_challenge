5f65d8c0b3fe476f11fbd4afa943bca53a7f8590
Chris Roman <Christopherroman85@gmail.com>
2020-03-24 11:57:49 -0500

    added controlled index route to take query string

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -6,8 +6,13 @@ module Api
 
 			# Get Route for all Transactions
 			def index
-				transactions = Transaction.order('created_at DESC')
-				render json: {status: 'Success!', message: 'Loaded All Transactions', data:transactions},status: :ok
+				if params[:query]
+					transactions = Transaction.find_by(api_key: params[:query])
+					render json: {status: 'Success!', message: 'Loaded All Transactions', data:transactions},status: :ok
+				else
+					transactions = Transaction.order('created_at DESC')
+					render json: {status: 'Success!', message: 'Loaded All Transactions', data:transactions},status: :ok
+				end
 			end
 
 		end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   		namespace 'v1' do
 
 			post 'coins/:query' => 'coins#create'
-			
+			get 'transactions/:query' => 'transactions#index'
+
 			resources :coins, only: [:index, :show, :create, :update, :destroy]
 
 			resources :transactions, only: [:index, :show, :create]
