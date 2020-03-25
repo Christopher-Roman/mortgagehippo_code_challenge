0af8284a3d66bfd8b1ff52eecaca240d648506ff
Chris Roman <Christopherroman85@gmail.com>
2020-03-25 15:10:43 -0500

    converted to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_25_165228) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "admins", force: :cascade do |t|
     t.string "email"
   end
