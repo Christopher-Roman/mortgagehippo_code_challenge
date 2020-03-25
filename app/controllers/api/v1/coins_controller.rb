1c756ba83dd079ca5c545386c1b0c25ea5241e87
Chris Roman <Christopherroman85@gmail.com>
2020-03-24 23:58:29 -0500

    started emailer, built counter

--- app/controllers/api/v1/coins_controller.rb
+++ app/controllers/api/v1/coins_controller.rb
@@ -24,17 +24,21 @@ module Api
 			def create
 				if (params[:query] == "food" || params[:query] == "bills" || params[:query] == "fun" || params[:query] == "savings")
 					coin = Coin.new(coin_params)
-					coin[:api_key] = params[:query]
-					if coin.save
-						transaction = Transaction.new(transaction_params)
-						transaction[:api_key] = params[:query]
-						if transaction.save
-							render json: {status: 'Success!', message: 'Saved Coins', data:coin},status: :ok
+					if(coin[:name] == "Adamentium Coin" || coin[:name] == "Carbonite Coin" || coin[:name] == "Amazonium Coin" || coin[:name] == "Vibranium Coin")
+						coin[:api_key] = params[:query]
+						if coin.save
+							transaction = Transaction.new(transaction_params)
+							transaction[:api_key] = params[:query]
+							if transaction.save
+								render json: {status: 'Success!', message: 'Saved Coins', data:coin},status: :ok
+							else
+								render json: {status: 'Error!', message: 'Transaction Not Saved', data:transaction.errors},status: :unprocessable_entity
+							end
 						else
-							render json: {status: 'Error!', message: 'Transaction Not Saved', data:transaction.errors},status: :unprocessable_entity
+							render json: {status: 'Error!', message: 'Coin Not Saved', data:coin.errors},status: :unprocessable_entity
 						end
 					else
-						render json: {status: 'Error!', message: 'Coin Not Saved', data:coin.errors},status: :unprocessable_entity
+						render json: {status: 'Error!', message: 'Must be one of the following coin names: Adamentium Coin, Carbonite Coin, Amazonium Coin, or Vibranium Coin'},status: :unprocessable_entity
 					end
 				else
 					render json: {status: 'Error!', message: 'Incorrect API Key', data:coin.errors},status: :unprocessable_entity
@@ -48,8 +52,12 @@ module Api
 				if coin.save
 					transaction = Transaction.new(name: coin.name, value:coin.value, api_key: coin.api_key, transaction_type: "Withdrawal")
 					if transaction.save
-						coin.destroy
-						render json: {status: 'Success!', message: 'Deleted Coin', data:coin},status: :ok
+						if coin.destroy
+							coins = Coin.order('created_at DESC')
+							@check = inventory_check
+
+							render json: {status: 'Success!', message: 'Deleted Coin', data:@check},status: :ok
+						end
 					else
 						render json: {status: 'Error!', message: 'Transaction Not Saved', data:transaction.errors},status: :unprocessable_entity
 					end
@@ -76,7 +84,24 @@ module Api
 			end
 
 
+			def inventory_check
+				adamentium = Coin.where("name = 'Adamentium Coin'").count
+				carbonite = Coin.where("name = 'Carbonite Coin'").count
+				amazonium = Coin.where("name = 'Amazonium Coin'").count
+				vibranium = Coin.where("name = 'Vibranium Coin'").count
+
+				all_coins = [adamentium, carbonite, amazonium, vibranium]
+
+				all_coins.each do |coins|
+					if coins < 4
+						#email function goes here
+						return "Inventory low, email sent."
+					end
+				end
+			end
+
 			private
+
 			# transaction object keys to verify before creating/editing coins
 
 			def transaction_params
